3a52e98fb0e3bbef871f490c71e374fcb0dc020e
Marina Reis <marinareis.mr@gmail.com>
2023-04-17 16:50:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
-import model.entites.Department;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,5 +12,9 @@ public class Program {
 		Department obj =  new Department(1, "Books");
 		
 		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Jose", "j@email.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 }
