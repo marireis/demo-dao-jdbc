4564f27240d22e5a182705e894ca79b024a8d2c5
Marina Reis <marinareis.mr@gmail.com>
2023-04-18 10:31:06 -0300

    insert Seller implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,5 +38,13 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TESTE 4: seller insert====");
+		Seller newSeller = new Seller(null, "Greg", "g@email.com", new Date(), 4000.0, department);
+
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserido! Novo id: " + newSeller.getId());
+		
+		
 	}
 }
